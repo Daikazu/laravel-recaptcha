e6f14693fac0bf33809de7e85fbd158f4b2d7caa
Mike Wall <daikazu@gmail.com>
2018-02-27 09:25:55 -0500

    wip

--- src/ReCaptchaServiceProvider.php
+++ src/ReCaptchaServiceProvider.php
@@ -39,6 +39,7 @@ class ReCaptchaServiceProvider extends ServiceProvider
     protected function bootConfig()
     {
         $path = __DIR__ . '/config/recaptcha.php';
+
         $this->mergeConfigFrom($path, 'recaptcha');
         if (function_exists('config_path')) {
             $this->publishes([$path => config_path('recaptcha.php')]);
